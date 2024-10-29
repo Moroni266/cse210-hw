d9754179dba70aeec26c9147eaa731cd29b7fe4a
Moroni266 <lop24005@byui.edu>
2024-10-29 11:15:13 -0600

    Add final project

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        // Console.WriteLine("Hello Foundation3 World!");
+        var running = new Running(new DateTime(2024, 10, 29), 30, 4.8);
+        var cycling = new Cycling(new DateTime(2024, 10, 29), 40, 20);
+        var swimming = new Swimming(new DateTime(2024, 10, 29), 25, 30);
+
+        List<Activity> activities = new List<Activity> { running, cycling, swimming };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
